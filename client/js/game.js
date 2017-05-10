413a252ec39fa924c8de4b94eb30c96c62f57ce2
Helltek <phinobow@gmail.com>
2017-05-09 23:49:33 -0400

    Add functionality

--- client/js/game.js
+++ client/js/game.js
@@ -1,4 +1,5 @@
-//Aliases
+PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST;
+
 var Application = PIXI.Application,
     Container = PIXI.Container,
     autoDetectRenderer = PIXI.autoDetectRenderer,
@@ -6,25 +7,57 @@ var Application = PIXI.Application,
     resources = PIXI.loader.resources,
     Sprite = PIXI.Sprite;
 
-var app = new Application(1024, 1024, { backgroundColor: 0xFFFFFF });
-document.body.appendChild(app.view)
+var app = new Application($(window).width() - 20, $(window).height() - 20, { backgroundColor: 0xAAAAAA });
 
-PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST;
+window.onresize = function (event) {
+    app.view.width = $(window).width() - 20;
+    app.view.height = $(window).height() - 20;
+};
+document.body.appendChild(app.view);
 
-var sprite = Sprite.fromImage('res/square.png');
+var main = Sprite.fromImage('res/square.png');
 
-sprite.anchor.set(0);
-sprite.x = 0;
-sprite.y = 0;
+main.anchor.set(0.5);
+main.x = 0; main.y = 0;
+app.stage.x = app.view.width / 2; app.stage.y = app.view.height / 2;
 
-sprite.interactive = true;
-sprite.buttonMode = true;
+main.interactive = true;
+main.buttonMode = true;
 
-sprite.on('pointerdown', onClick);
+main
+    .on('pointerdown', startDrag)
+    .on('pointerup', endDrag)
+    .on('pointerupoutside', endDrag)
+    .on('pointermove', moveDrag);
 
-app.stage.addChild(sprite);
+app.stage.addChild(main);
 
-function onClick () {
-    sprite.width += 10;
-    sprite.height += 10;
+function startDrag(event) {
+    main.width += 10; main.height += 10;
+
+    this.data = event.data;
+    this.dragging = true;
+
+    this.initStagePosition = app.stage.position;
+    this.initMousePosition = this.data.getLocalPosition(this.parent);
+}
+
+function endDrag() {
+    this.data = null;
+    this.dragging = false;
+}
+
+function moveDrag() {
+    if (this.dragging) {
+        var dx = this.initMousePosition.x - this.data.getLocalPosition(this.parent).x,
+            dy = this.initMousePosition.y + this.data.getLocalPosition(this.parent).y;
+
+        var newX = this.initStagePosition.x + dx/3,
+            newY = this.initStagePosition.y + dy/3;
+
+        if (main.width > app.view.width) app.stage.position.x = (newX < main.width / 2 && newX > app.view.width - main.width / 2) ? newX : app.stage.position.x;
+        if (main.height > app.view.height) app.stage.position.y = (newY < main.height / 2 && newY > app.view.height - main.height / 2) ? newY : app.stage.position.y;
+
+        this.initMousePosition = this.data.getLocalPosition(this.parent);
+    }
 }
