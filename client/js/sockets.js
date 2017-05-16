13666bae52d73b5288259df9d472ae5803474ca6
Helltek <phinobow@gmail.com>
2017-05-16 06:25:45 -0400

    Integrate Google sign-in on client

--- client/js/sockets.js
+++ client/js/sockets.js
@@ -1,14 +1,16 @@
 var ws = new WebSocket("ws://96.239.90.68:25565");
 
 ws.onmessage = function(event) {
+    if (event.data == '{"auth": false}') return false;
     console.log(event);
     //$.getScript("js/game.js")
 };
 
-function login() {
-    user = $('[name="user"]')[0].value;
-    pass = $('[name="passwd"]')[0].value;
+ws.onopen = function(event) {
+    console.log("connected");
+}
 
-    data =  JSON.stringify( {"login": {"user": user, "pass": pass}} );
-    ws.send(data);
+function onSignIn(googleUser) {
+    console.log("signed in");
+    ws.send(JSON.stringify( {"login":googleUser.getAuthResponse().id_token}) );
 }
