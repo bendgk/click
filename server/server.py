281501d8aefa256c1a3502006c1a386694637660
Benjamin Kosten <benkosten@gmail.com>
2017-05-16 14:45:21 -0400

    Redesigned backend
    Intergrated google authentication with server. Added a listener to the client class for a more organized way to communicate client <---> server.

--- server/server.py
+++ server/server.py
@@ -1,13 +1,58 @@
-import asyncio
-import websockets
+import asyncio, websockets, json
+from oauth2client import client, crypt
+
+class Player:
+    def __init__(self, userid, data):
+        self.userid = userid
+        self.clicks = data['clicks']
 
 class Client:
     def __init__(self, ws, path):
         self.ws = ws
-        self.ra = ws.remote_address
         self.path = path
-        self.clicks = 0
 
+    async def listen(self):
+        while True:
+            #Process data from server to usable format
+            data = await self.ws.recv()
+            data = json.loads(data)
+            key = list(data.keys())[0]
+            data = data[key]
+
+            #Call appropriate method
+            method = getattr(self, key)
+            execute = asyncio.ensure_future(method(data))
+
+    async def login(self, data):
+        try:
+            idinfo = client.verify_id_token(data, None)
+
+            if idinfo['iss'] not in ['accounts.google.com', 'https://accounts.google.com']:
+                raise crypt.AppIdentityError("Wrong issuer.")
+
+            userid = idinfo['sub']
+
+            if userid not in server.users:
+                #Creation of user in users.json
+                server.users[userid] = {'clicks': 0}
+
+                with open('users.json', 'w') as f:
+                    json.dump(server.users, f)
+                    f.close()
+
+            player = Player(usserid, server.users[userid])
+            await self.ws.send(json.dumps(server.users[userid]))
+
+        except crypt.AppIdentityError:
+            await self.ws.send(json.dumps({'auth': False}))
+
+    async def click(self, data):
+        player.clicks += 1
+        await asyncio.sleep(0.1)
+        self.ws.send(json.dumps({'clicks': player.clicks}))
+
+
+    """
     async def register(self):
         server.users.add(self.ws)
         print(self.ra, "joined")
@@ -44,17 +89,18 @@ class Client:
 
         except Exception as e:
             pass
+        """
 
 class Server(websockets.server.WebSocketServerProtocol):
     def __init__(self):
-        self.users = set()
+        with open('users.json') as users:
+            self.users = json.load(users)
+            users.close()
 
     async def handler(self, ws, path):
         client = Client(ws, path)
-        register = asyncio.ensure_future(client.register())
-        click = asyncio.ensure_future(client.click())
-
-        await asyncio.wait([register, click])
+        listener = asyncio.ensure_future(client.listen())
+        await asyncio.wait([listener])
 
     def run(self, host='192.168.1.9', port=25565):
         start_server = websockets.serve(self.handler, host, port)
